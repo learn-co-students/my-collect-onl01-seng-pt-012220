56354c98d51fd377f7e27dfe8e5d3634acfc54d6
Xavier Perez <xperez007@gmail.com>
2020-01-28 13:52:36 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,13 @@
+array = ["Tim Jones", "Tom 
+Smith", "Jim Campagno"]
+my_collection(array) do|name|
+  name.split(" ").first 
+end
+
+collection = ['ruby', 'javascript', 'python', 'objective-c']
+my_collection(collection) do
+|lang|
+  lang.upcase
+end
 
 
