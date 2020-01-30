1b9e6878663cc31eaf2dc3f8f4e741a26276daa4
Chris Benedict <cabx88@gmail.com>
2020-01-30 02:45:36 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,12 @@
+def my_collect(language)
+  counter = 0
+  updated_language = []
 
+  while counter < language.length
+    updated_language << yield(language[counter])
+    counter += 1
+  end
+  updated_language
+end
 
+my_collect(["ruby", "javascript", "python", "objective-c"]) {|language| language.upcase}
